9b16ad1eb07c1cc8aef014ceb9b6ea7b7f97f1f1
acer <s160421085@student.ubaya.ac.id>
2024-06-11 20:36:25 +0700

    Login DONE all

--- app/src/main/java/com/example/uas_anmp/view/LoginFragment.kt
+++ app/src/main/java/com/example/uas_anmp/view/LoginFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.uas_anmp.R
 import com.example.uas_anmp.databinding.FragmentLoginBinding
+import com.example.uas_anmp.model.User
 import com.example.uas_anmp.viewmodel.UserViewModel
 
 class LoginFragment : Fragment() {
@@ -23,8 +27,23 @@ class LoginFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding.btnToRegister.setOnClickListener(){
-            findNavController().navigate(R.id.actionloginToregis)
+        viewmodel = ViewModelProvider(this).get(UserViewModel::class.java)
+
+        viewmodel.loginSuccess.observe(viewLifecycleOwner) { success ->
+            if (success) {
+
+                findNavController().navigate(R.id.actionloginTolistHobby)
+            } else {
+                Toast.makeText(requireContext(), "Username atau Password Salah", Toast.LENGTH_SHORT).show()
+            }
         }
+
+        binding.btnLogin.setOnClickListener {
+            val username = binding.txtusernameLGN.text.toString()
+            val password = binding.txtpasswordLGN.text.toString()
+            viewmodel.login(username, password)
+        }
+
     }
+
 }
--- app/src/main/java/com/example/uas_anmp/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/example/uas_anmp/viewmodel/UserViewModel.kt
@@ -3,9 +3,12 @@ package com.example.uas_anmp.viewmodel
     import android.app.Application
     import android.widget.Toast
     import androidx.lifecycle.AndroidViewModel
+    import androidx.lifecycle.LiveData
+    import androidx.lifecycle.MutableLiveData
     import androidx.lifecycle.viewModelScope
     import com.example.uas_anmp.model.User
-import com.example.uas_anmp.model.UserDatabase
+    import com.example.uas_anmp.model.UserDao
+    import com.example.uas_anmp.model.UserDatabase
 import com.example.uas_anmp.util.buildDB
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,7 +19,6 @@ import kotlinx.coroutines.launch
 
 class UserViewModel(application: Application)
     : AndroidViewModel(application), CoroutineScope {
-
     private val job = Job()
     fun addUser(user: User) {
         viewModelScope.launch {
@@ -43,12 +45,27 @@ class UserViewModel(application: Application)
           //  db.userDao().insertAll(user)
         //}
     //}
-    fun loginUser(username:String,password:String){
-        launch {
+    private val _loginSuccess = MutableLiveData<Boolean>()
+    val loginSuccess: LiveData<Boolean>
+        get() = _loginSuccess
+    fun login(username: String, password: String) {
+        viewModelScope.launch {
             val db = buildDB(getApplication())
-            db.userDao().loginUser(username, password)
+            val user = withContext(Dispatchers.IO) {
+                db.userDao().loginUser(username, password)
+            }
+            if (user != null) {
+                // Handle successful login
+                _loginSuccess.value = true
+            } else {
+                // Handle login failure
+                _loginSuccess.value = false
+            }
         }
     }
+
+
+
     override val coroutineContext: CoroutineContext
         get() = job + Dispatchers.IO
 }
