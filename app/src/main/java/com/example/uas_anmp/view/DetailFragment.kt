82e19618be2a232066fe7209068e680752312d7f
acer <s160421085@student.ubaya.ac.id>
2024-06-14 23:49:38 +0700

    fix DetailFragment

--- app/src/main/java/com/example/uas_anmp/view/DetailFragment.kt
+++ app/src/main/java/com/example/uas_anmp/view/DetailFragment.kt
@@ -1,7 +1,45 @@
 package com.example.uas_anmp.view
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import com.example.uas_anmp.R
+import com.example.uas_anmp.databinding.FragmentDetailBinding
+import com.example.uas_anmp.viewmodel.DetailHobbyViewModel
+import com.example.uas_anmp.viewmodel.ListHobbyViewModel
 
+class DetailFragment : Fragment() {
+
+    private lateinit var viewModel: DetailHobbyViewModel
+    private lateinit var databinding: FragmentDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        databinding = DataBindingUtil.inflate<FragmentDetailBinding>(inflater,R.layout.fragment_detail,container,false)
+        // Inflate the layout for this fragment
+        return databinding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(DetailHobbyViewModel::class.java)
+        val uuid = DetailFragmentArgs.fromBundle(requireArguments()).uuid
+        viewModel.fetch(uuid)
+
+
+        observerViewModel()
+    }
+    fun observerViewModel() {
+        viewModel.hobbyLD.observe(viewLifecycleOwner, Observer {
+            databinding.hobby = it
+        })
+    }
+
+
+}
